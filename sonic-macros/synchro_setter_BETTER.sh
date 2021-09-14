2ce1de1b8ac14af7b6215e98f0df5cb158c594af
William McCormack <wmccorma@mit.edu>
2021-09-14 12:33:02 -0500

    fixed synchro_setter_BETTER

--- sonic-macros/synchro_setter_BETTER.sh
+++ sonic-macros/synchro_setter_BETTER.sh
@@ -6,7 +6,7 @@ CLIENTS=$(condor_q ${CLUSTER} -af READY | wc | awk {'print $1'})
 while true
 do
     NCLIENTS_RUNNING=$(condor_q ${CLUSTER} -af READY | grep True | wc | awk {'print $1'})
-    if [[ ${NCLIENTS} == ${NCLIENTS_RUNNING} ]]
+    if [[ ${CLIENTS} == ${NCLIENTS_RUNNING} ]]
     then
 	condor_qedit ${CLUSTER} GO '"TRUE"'
 	break
